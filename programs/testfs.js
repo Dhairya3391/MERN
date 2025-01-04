49db298e9d6677caa847b60f7dbaa58193308489
Dhairya3391 <dhairyaadroja3391@gmail.com>
2025-01-04 19:41:46 +0530

    idk

--- programs/testfs.js
+++ programs/testfs.js
@@ -1,1 +1,14 @@
-787
+const some = require('express')
+
+const app = some()
+
+app.get('/some',(req,res)=>{
+    res.send(`<br>hello world</br>`)
+})
+
+app.listen(3000,()=>{
+    console.log("some");
+    
+})
+
+Function(some)
