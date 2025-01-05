9ac046a5ff94f6767c84040ff832ca33f644ba52
Dhairya3391 <dhairyaadroja3391@gmail.com>
2025-01-05 21:52:26 +0530

    idk

---
+++ leetcode.js
@@ -0,0 +1,8 @@
+<html>
+  <head>
+    <title>Leetcode</title>
+  </head>
+  <body>
+    <script src="leetcode.js" />
+  </body>
+</html>;
--- programs/lab4.js
+++ programs/lab4.js
@@ -46,4 +46,38 @@ let foo = bar => { console.log(bar,"happened"); }
 
 eventEmitter.on('baz',(bax)=>{foo(bax)})
 
+const quickSort = (arr) => {
+  if (arr.length <= 1) return arr;
+  const pivot = arr.shift();
+  const left = arr.filter((el) => el < pivot);
+  const right = arr.filter((el) => el >= pivot);
+  return quickSort(left).concat(pivot, quickSort(right));
+};
+
+TraditionalquickSort([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+const TraditionalquickSort = (arr, low = 0, high = arr.length - 1) => {
+    if (low < high) {
+      const pivotIndex = partition(arr, low, high);
+      quickSort(arr, low, pivotIndex - 1);
+      quickSort(arr, pivotIndex + 1, high);
+    }
+    return arr;
+  };
+  
+  const partition = (arr, low, high) => {
+    const pivot = arr[high];
+    let i = low - 1;
+    
+    for (let j = low; j < high; j++) {
+      if (arr[j] <= pivot) {
+        i++;
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+      }
+    }
+    
+    [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]];
+    return i + 1;
+  };
+
 eventEmitter.emit('baz','coding')
